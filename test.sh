0294f826d3c3bda78c2a3fcb1952182c8d3b8768
huananana <940665932@qq.com>
2019-12-28 14:42:57 +0800

    test

---
+++ test.sh
@@ -0,0 +1,23 @@
+﻿# 使用shell脚本编写自动更新仓库脚本
+
+# 移动到您的本地仓库地址
+cd /d/studyNote
+echo 请输入提示信息：
+read submit_information
+
+# 添加缓存区
+git add -A
+
+# 查看git状态和提交日记
+echo ========git状态=========
+git status
+
+# 提交本地仓库
+git commit -m $submit_information
+
+# 查看提交日记
+echo ========提交日记=========
+#git log
+
+# 上传到远程仓库master分之
+git push
